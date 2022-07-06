470417800e49e3c51fc83dd04e93a11552ee81de
felix1805 <felix1805@gmail.com>
2022-07-06 10:34:04 -0400

    initial react app file structure with npx install with firebase login

---
+++ messenger-app/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDPKONCsEfpQr_4lBfOHges8pdThxDAqks",
+  authDomain: "messenger-app-7c09d.firebaseapp.com",
+  projectId: "messenger-app-7c09d",
+  storageBucket: "messenger-app-7c09d.appspot.com",
+  messagingSenderId: "829474581752",
+  appId: "1:829474581752:web:0f32be5f3f2283ef9e5f0d",
+  measurementId: "G-D1SJ3NF0PH"
+};
