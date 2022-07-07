3be305a82824e7e63593e3cf28c3fe0820a385ce
felix1805 <felix1805@gmail.com>
2022-07-07 10:30:12 -0400

    added initial css structure

---
+++ messenger-app/src/Sidebar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Sidebar.css';
+import { Avatar } from '@mui/material';
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar_header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <DonutLargeIcon />
+          <ChatIcon />
+          <MoreVertIcon />  
+
+        </div>
+      </div>
+      <div className="sidebar_search">
+
+      </div>
+      <div className="sidebar_chats">
+
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
