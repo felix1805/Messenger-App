7628dfcbc180b3a4b947276b7c075c19a6fec0e7
felix1805 <felix1805@gmail.com>
2022-07-08 11:27:54 -0400

    added chat functionality and styling

--- messenger-app/src/Sidebar.js
+++ messenger-app/src/Sidebar.js
@@ -5,6 +5,7 @@ import DonutLargeIcon from '@mui/icons-material/DonutLarge';
 import ChatIcon from '@mui/icons-material/Chat';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import SearchOutlined from '@mui/icons-material/SearchOutlined';
+import SidebarChat from './SidebarChat';
 
 
 function Sidebar() {
@@ -31,7 +32,12 @@ function Sidebar() {
           <input placeholder='Search or start new chat' type='text' />
         </div>
       </div>
-      <div className="sidebar__chats"></div>
+      <div className="sidebar__chats">
+        <SidebarChat addNewChat/>
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+      </div>
     </div>
   );
 }
