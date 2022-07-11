2f103b7fc36c27ca1be649ff15f09d56afed2bbb
felix1805 <felix1805@gmail.com>
2022-07-11 14:02:23 -0400

    finalized firebase database and ran final build

--- messenger-app/src/Chat.js
+++ messenger-app/src/Chat.js
@@ -8,20 +8,30 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from "react-router-dom"
 import "./Chat.css"
 import db from './firebase';
+import firebase from 'firebase';
+import { useStateValue } from './StateProvider';
 
 function Chat() {
   const [input, setInput] = useState("")
   const [seed, setSeed] = useState("")
   const { roomId } = useParams();
   const [roomName, setRoomName] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [{ user }, dispatch] = useStateValue();
 
   useEffect(() => {
     if (roomId) {
-      db.collection('rooms').doc(roomId).onSnapshot(snapshot => (
-        setRoomName(snapshot.data().name)
-      ))
+      db.collection('rooms')
+        .doc(roomId)
+        .onSnapshot((snapshot) => setRoomName
+          (snapshot.data().name));
+
+      db.collection('rooms').doc(roomId).
+        collection('messages').orderBy('timestamp', 'asc').onSnapshot(snapshot => (
+          setMessages(snapshot.docs.map(doc => doc.data()))
+        ))
     }
-  }, [roomId])
+  }, [roomId]);
 
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000))
@@ -31,6 +41,13 @@ function Chat() {
 
     e.preventDefault();
     console.log("Yout typed", input);
+
+    db.collection('rooms').doc(roomId).collection('messages').add( {
+      message: input,
+      name: user.displayName,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
     setInput('');
   };
 
@@ -40,7 +57,8 @@ function Chat() {
         <Avatar src={`https://avatars.dicebear.com/api/bottts/${seed}.svg`} />
         <div className="chat__headerInfo">
           <h3>{roomName}</h3>
-          <p>Last seen at ...</p>
+          <p>Last seen {''}
+          {new Date(messages[messages.length -1]?.timestamp?.toDate()).toUTCString()}</p>
         </div>
         <div className="chat__headerRight">
           <IconButton>
@@ -55,13 +73,16 @@ function Chat() {
         </div>
       </div>
       <div className="chat__body">
-        <p className={`chat__message ${true && 'chat__receiver'}`}>
-          <span className="chat__name">
-            Felix Petzsche
-          </span>
-          Hey Guys
-          <span className="chat__timestamp">3:52pm</span>
-        </p>
+        {messages.map((message) => (
+          <p className={`chat__message ${message.name === user.displayName && 'chat__receiver'}`}>
+            <span className="chat__name">
+              {message.name}</span>
+            {message.message}
+            <span className="chat__timestamp">
+              {new Date(message.timestamp?.toDate()).toUTCString()}
+            </span>
+          </p>
+        ))}
       </div>
 
       <div className="chat__footer">
--- messenger-app/src/SidebarChat.js
+++ messenger-app/src/SidebarChat.js
@@ -6,6 +6,14 @@ import { Link } from "react-router-dom";
 
 function SidebarChat({ id, name, addNewChat }) {
   const [seed, setSeed] = useState('')
+  const [messages, setMessages] = useState('');
+
+  useEffect(() => {
+    if (id) {
+      db.collection('rooms').doc(id).collection('messages').orderBy('timestamp', 'desc').onSnapshot((snapshot) => setMessages(snapshot.docs.map((doc) => doc.data()))
+      );
+    }
+  }, [id]);
 
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000))
@@ -15,20 +23,20 @@ function SidebarChat({ id, name, addNewChat }) {
     const roomName = prompt("Please enter name for chat");
 
     if (roomName) {
-db.collection('rooms').add({
-  name: roomName,
-})
+      db.collection('rooms').add({
+        name: roomName,
+      })
     }
   };
   return !addNewChat ? (
     <Link to={`/rooms/${id}`}>
-    <div onClick={createChat} className='sidebarChat'>
-      <Avatar src={`https://avatars.dicebear.com/api/bottts/${seed}.svg`} />
-      <div className='sidebarChat__info'>
-        <h2>{name}</h2>
-        <p>Last message...</p>
+      <div onClick={createChat} className='sidebarChat'>
+        <Avatar src={`https://avatars.dicebear.com/api/bottts/${seed}.svg`} />
+        <div className='sidebarChat__info'>
+          <h2>{name}</h2>
+          <p>{messages[0]?.message}</p>
+        </div>
       </div>
-    </div>
     </Link>
   ) : (
     <div onClick={createChat}
@@ -38,4 +46,4 @@ db.collection('rooms').add({
   );
 }
 
-export default SidebarChat
+export default SidebarChat;
